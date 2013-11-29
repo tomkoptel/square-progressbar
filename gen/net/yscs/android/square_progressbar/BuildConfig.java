e6bc3dcc71686582d4a23a7c59837216aa34cd3b
Tom Koptel <tom.koptel@gmail.com>
2013-11-29 08:32:35 +0200

    Initial commit

---
+++ gen/net/yscs/android/square_progressbar/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package net.yscs.android.square_progressbar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ target/generated-sources/r/net/yscs/android/square_progressbar/BuildConfig.java
@@ -0,0 +1,5 @@
+package net.yscs.android.square_progressbar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
